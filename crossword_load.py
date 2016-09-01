892ca4a659daa41f018861261335075c013d7e52
Tom Molinari <tom.molinari12@gmail.com>
2016-09-01 02:51:10 -0400

    Figured out how to iterate over the grid

--- crossword_load.py
+++ crossword_load.py
@@ -7,4 +7,6 @@ for year in years[:1]:
 	for puzzle in puzzles_list[:1]:
 		puzzle_text = open(str(year)+"/"+puzzle,"r").read()
 		puzzle_soup = BeautifulSoup(puzzle_text, "lxml")
-		print puzzle_soup
+		grid = puzzle_soup.findAll("grid")[0]
+		print grid.findAll("row")[0].contents[0]
+
