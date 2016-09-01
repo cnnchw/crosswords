e0fbc6b45263b8e2ac13c22a85eb5d6875479ba1
Tom Molinari <tom.molinari12@gmail.com>
2016-09-01 02:40:04 -0400

    no message

--- crossword_load.py
+++ crossword_load.py
@@ -1,2 +1,10 @@
-import BeautifulSoup4, pandas
+from bs4 import BeautifulSoup 
+import pandas, os
 
+years = [2015,2016]
+for year in years[:1]:
+	puzzles_list = os.listdir(str(year))
+	for puzzle in puzzles_list[:1]:
+		puzzle_text = open(str(year)+"/"+puzzle,"r").read()
+		puzzle_soup = BeautifulSoup(puzzle_text, "lxml")
+		print puzzle_soup
